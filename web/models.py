5feffc364358c04ec7a610e63319c54b5ccae079
Mousavi <158860308+odriew3j@users.noreply.github.com>
2024-10-09 19:13:47 +0330

    income expense models and some beatification

--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+from django.db import models  
+from django.contrib.auth.models import User  
+from django.utils import timezone  
 
-# Create your models here.
+class Expense(models.Model):  
+    text = models.CharField(max_length=255)  
+    date = models.DateTimeField()  
+    amount = models.BigIntegerField()  
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  
+
+    def __str__(self):  
+        return "{} - {}".format(self.date, self.amount)  
+
+class Income(models.Model):  
+    text = models.CharField(max_length=255)  
+    date = models.DateTimeField()  
+    amount = models.BigIntegerField()  
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  
+
+    def __str__(self):  
+        return "{} - {}".format(self.date, self.amount)
